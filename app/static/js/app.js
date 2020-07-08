6f6980630c283bff9e81e213c1975dfd8c9af81a
Raihan Yudo Saputra <raihanyudosaputra30@gmail.com>
2020-07-08 23:55:33 +0700

    add loading page

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -9,9 +9,16 @@ $(document).ready(function () {
     $.ajax({
       type: "get",
       url: "/start_broadcast",
+      beforeSend: function (xhr) {
+        showLoading();
+      },
       success: function (response) {
         $("#status_live").text("Running");
         $("#msg_live").val(response.message);
+        hideLoading();
+      },
+      error: function (response) {
+        hideLoading();
       },
     });
   });
@@ -20,10 +27,31 @@ $(document).ready(function () {
     $.ajax({
       type: "get",
       url: "/stop_broadcast",
+      beforeSend: function (xhr) {
+        showLoading();
+      },
       success: function (response) {
         $("#status_live").text("Stopped");
         $("#msg_live").val(response.message);
+        hideLoading();
+      },
+      error: function (response) {
+        hideLoading();
       },
     });
   });
 });
+
+function showLoading() {
+  $("#dashboard_action").LoadingOverlay("show", {
+    background: "rgba(0, 0, 0, 0.5)",
+    imageColor: "#fff",
+  });
+}
+
+function hideLoading() {
+  $("#dashboard_action").LoadingOverlay("hide", {
+    background: "rgba(0, 0, 0, 0.5)",
+    imageColor: "#fff",
+  });
+}
