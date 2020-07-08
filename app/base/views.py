af409f8d6353d492ae065538e827e6b9a809db8f
Raihan Yudo Saputra <raihanyudosaputra30@gmail.com>
2020-07-08 22:56:34 +0700

    add create server, and status

--- app/base/views.py
+++ app/base/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, request, session
+from flask import Blueprint, render_template, redirect, url_for, request, session, flash
 from ItsAGramLive import ItsAGramLive
 import json
 from app.utils import fromPickle, toPickle
@@ -10,15 +10,22 @@ def login_route():
 
 @base.route('/dashboard')
 def info_route():
-    return render_template('pages/dashboard.html')
+    return render_template('pages/dashboard.html',data_stream=session['data_stream'])
 
 @base.route('/login', methods=['POST'])
 def login_handle():
     live = ItsAGramLive(username=request.form['username'],password=request.form['password'])
     live.login()
-    
+    live.create_broadcast()
     # convert to pickle and store it on file
     toPickle(live)
+
+    session['data_stream'] = {
+        'broadcast_id':live.broadcast_id,
+        'stream_server':live.stream_server,
+        'stream_key':live.stream_key
+    }
     if live.isLoggedIn:
+        flash("You're not live, start broadcast after you set the server key")
         return redirect(url_for('base.info_route'))
     return redirect(url_for('base.login_route'))
